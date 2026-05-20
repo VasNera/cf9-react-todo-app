06c1e2f756b3958f61d20e7b634ab56f50e09301
Vas. Neratzis <vas.neratzis@gmail.com>
2026-05-20 19:21:26 +0300

    feat: add types, layout, header, footer

---
+++ src/shared/layout/Layout.tsx
@@ -0,0 +1,19 @@
+import Footer from './Footer'
+import Header from './Header'
+import type{LayoutProps} from './types.ts'
+
+
+
+const Layout = ({children} : LayoutProps) =>{
+    return (
+        <>
+            <Header/>
+            <main className="container mx-auto min-h-[95vh] pt-36">{children}</main>
+
+            <Footer/>
+
+        </>
+    )
+}
+
+export default Layout;
